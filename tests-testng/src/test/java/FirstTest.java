f6f9ff61569221615c61d6729306eef0c6a704c3
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2020-01-19 22:16:00 +0300

    homework ver 0.1 lesson-1

--- tests-testng/src/test/java/FirstTest.java
+++ tests-testng/src/test/java/FirstTest.java
@@ -1,17 +1,16 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class FirstTest {
-    private static final Logger logger = LogManager.getLogger(FirstTest.class);
-    public WebDriverManager webDriverManager;
-    public static void main(String[] args) {
 
-        logger.info("INFO");
-        logger.debug("DEBUG");
-        logger.error("ERROR");
-        logger.fatal("FATAL");
-        logger.warn("WARN");
-    }
+public class FirstTest extends TestBase{
+  private static final Logger logger = LogManager.getLogger();
 
+  public static void main(String[] args) {
+    System.out.println("START");
+    logger.info("Поехали!");
+    app.init();
+    logger.info("Приехали");
+    app.quit();
+    logger.info("Стоп машина");
+  }
 }
