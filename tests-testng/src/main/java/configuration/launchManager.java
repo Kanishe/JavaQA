0678384177963a2b50a8d2ca7d01e82c0925f9dd
Alexandr Kanishevskii <aka@MacBook-Pro-Alexandr.local>
2020-02-02 18:30:27 +0300

    homework ver 2 fix all class and log4j2

---
+++ tests-testng/src/main/java/configuration/launchManager.java
@@ -0,0 +1,37 @@
+package configuration;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class launchManager {
+    public WebDriver driver;
+    private static final Logger logger = LogManager.getLogger(launchManager.class);
+
+
+    @BeforeMethod
+    public void setupBrowser(){
+        String browser = BrowserType.CHROME;
+        if (browser == BrowserType.CHROME){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            logger.info("Selected Browser - Chrome");
+        } else if(browser == BrowserType.FIREFOX) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+            logger.info("Selected Browser - Firefox");
+        }
+    }
+    @AfterMethod
+    public void terDown(){
+        if(driver != null){
+            driver.quit();
+        }
+    }
+}
