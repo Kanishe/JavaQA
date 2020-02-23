f06865e52e40f407f6df3f01f9a5f3ff6af14f9f
Kanishe <50548979+Kanishe@users.noreply.github.com>
2020-02-23 22:07:46 +0300

    add Lesson 4

--- Lesson4/src/test/java/WebDriverFactory.java
+++ Lesson4/src/test/java/WebDriverFactory.java
@@ -1,4 +1,10 @@
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
 
 public class WebDriverFactory {
+    public static final Logger logger = LogManager.getLogger(WebDriverFactory.class);
+
 }
+
+
---
+++ Lesson4/src/test/java/WebDriverName.java
@@ -0,0 +1,6 @@
+public class WebDriverName extends WebDriverFactory{
+    public static void main(String[] args) {
+        System.out.println("dd");
+        logger.info("ff");
+    }
+}
